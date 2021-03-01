fddf72fc283c336ff17df1e3b1c040eca7f81993
Aaron Mason <apmason13@gmail.com>
2021-03-01 14:08:11 -0500

    Worked on Lego Script
    - updated lego measurement info
    - added some reference images and websites to the references folder
    - created the initial lego script

---
+++ Assets/Scripts/Lego/Lego.cs
@@ -0,0 +1,63 @@
+﻿using Tilia.Interactions.Interactables.Interactables;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InteractableFacade))]
+[RequireComponent(typeof(Rigidbody))]
+public class Lego : MonoBehaviour
+{
+    private InteractableFacade interactableFacade;
+    private Rigidbody rbody;
+    Collider coll;
+    private LayerMask legoLayerMask;
+    private GameObject legoToSnapTo;
+    public float maxSnappableDistance = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactableFacade = GetComponent<InteractableFacade>();
+        rbody = GetComponent<Rigidbody>();
+        coll = GetComponentInChildren<BoxCollider>();
+        legoLayerMask = LayerMask.GetMask("Lego");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (interactableFacade.IsGrabbed)
+        {
+            Ray ray = new Ray(transform.position + new Vector3(0.1f, -0.001f, 0.1f), Vector3.down * 5);
+            RaycastHit hit;
+            Debug.DrawRay(ray.origin, ray.direction, Color.red);
+
+            if (Physics.Raycast(ray, out hit, maxSnappableDistance, legoLayerMask))
+            {
+                legoToSnapTo = hit.collider.gameObject;
+            }
+        }
+    }
+
+    public void OnLegoGrab()
+    {
+        Debug.Log("Lego Grabbed");
+        rbody.useGravity = true;
+        coll.isTrigger = false;
+
+        legoToSnapTo = null;
+    }
+
+    public void onLegoUngrab()
+    {
+
+        Debug.Log("Lego Ungrabbed");
+        if (legoToSnapTo != null)
+        {
+            rbody.useGravity = false;
+            coll.isTrigger = true;
+            transform.position = legoToSnapTo.transform.position + new Vector3(0, 0.24f, 0);
+            transform.rotation = legoToSnapTo.transform.rotation;
+        }
+    }
+}
---
+++ Assets/Scripts/LegoMeasurements/LegoMeasurements.cs
@@ -0,0 +1,106 @@
+public static class LegoMeasurements
+{
+
+    // This class contains conversion ratios to convert between the different units. 
+    //Simply multiply a measurement of the left unit to obtain a measurement of the right unit.
+    public static class CONVERSIONS
+    {
+        public const double METER_TO_MILLIMETER = 0.001;
+        public const double METER_TO_LDU = 0.0004;
+        public const double MILLIMETER_TO_METER = 1000;
+        public const double MILLIMETER_TO_LDU = 0.4;
+        public const double LDU_TO_MILLIMETER = 2.5;
+        public const double LDU_TO_METER = 2500;
+    }
+
+    // Measurements
+    public static class METER
+    {
+        /**************************/
+        /* 1x1 Lego Measurements */
+        /**************************/
+        // Standard Lego Measurements
+        public const double LEGO_WIDTH = 0.008; // X or Z Size
+        public const double LEGO_DEPTH = 0.008; // X or Z Size
+
+        // Stud Measurements
+        public const double STUD_HEIGHT = 0.0016;
+        public const double STUD_RADIUS = 0.0024;
+        public const double STUD_DIAMETER = 0.0048;
+
+        // distances between two adjacent studs
+        public const double DISTANCE_BETWEEN_STUD_CENTERS = 0.008;
+        public const double DISTANCE_BETWEEN_STUD_EDGES = 0.0032;
+
+        /**************************/
+        /* 1x1 Brick Measurements */
+        /**************************/
+        public const double BRICK_HEIGHT = 0.0096; // Y Size
+
+        /**************************/
+        /* 1x1 Plate Measurements */
+        /**************************/
+        public const double PLATE_HEIGHT = 0.0032; // Y Size
+    }
+    public class MILLIMETER
+    {
+        /**************************/
+        /* 1x1 Lego Measurements */
+        /**************************/
+        // Standard Lego Measurements
+        public const double LEGO_WIDTH = 8; // X or Z Size
+        public const double LEGO_DEPTH = 8; // X or Z Size
+
+        // Stud Measurements
+        public const double STUD_HEIGHT = 1.6;
+        public const double STUD_RADIUS = 2.4;
+        public const double STUD_DIAMETER = 4.8;
+
+        // distances between two adjacent studs
+        public const double DISTANCE_BETWEEN_STUD_CENTERS = 8;
+        public const double DISTANCE_BETWEEN_STUD_EDGES = 3.2;
+
+        /**************************/
+        /* 1x1 Brick Measurements */
+        /**************************/
+        public const double BRICK_HEIGHT = 9.6; // Y Size
+
+        /**************************/
+        /* 1x1 Plate Measurements */
+        /**************************/
+        public const double PLATE_HEIGHT = 3.2; // Y Size
+    }
+    public class LDU
+    {
+        /* 
+          LDU is a simple unit that can simplify the measurements of various lego measurements.
+          1 LDU = 0.4 mm;   
+        */
+
+        /**************************/
+        /* 1x1 Lego Measurements  */
+        /**************************/
+        // Standard Lego Measurements
+        public const double LEGO_WIDTH = 20; // X or Z Size
+        public const double LEGO_DEPTH = 20; // X or Z Size
+
+        // Stud Measurements
+        public const double STUD_HEIGHT = 4;
+        public const double STUD_RADIUS = 6;
+        public const double STUD_DIAMETER = 12;
+
+        // distances between two adjacent studs
+        public const double DISTANCE_BETWEEN_STUD_CENTERS = 40;
+        public const double DISTANCE_BETWEEN_STUD_EDGES = 8;
+
+        /**************************/
+        /* 1x1 Brick Measurements */
+        /**************************/
+        public const double BRICK_HEIGHT = 24; // Y Size
+
+        /**************************/
+        /* 1x1 Plate Measurements */
+        /**************************/
+        public const double PLATE_HEIGHT = 8; // Y Size
+    }
+}
