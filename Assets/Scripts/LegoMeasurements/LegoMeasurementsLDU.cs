632dfc9c81c8d14388b2b3dd168bbcd36474c3f6
Aaron Mason <apmason13@gmail.com>
2021-03-01 10:10:56 -0500

    Created Lego Test Scene / Added Lego Measurement Info

---
+++ Assets/Scripts/LegoMeasurements/LegoMeasurementsLDU.cs
@@ -0,0 +1,38 @@
+﻿public class LegoInfoLDU
+{
+    /* 
+      Below are defined sizes for lego pieces. 
+      All measurements are in LDU.
+      LDU is a simple unit that can simplify the measurements of various lego measurements.
+      1 LDU = 0.4 mm;   
+    */
+
+    // conversion from ldu to mm
+    public const double LDU_TO_MM_RATIO = 2.5;
+
+    /**************************/
+    /* 1x1 Lego Measurements  */
+    /**************************/
+    // Standard Lego Measurements
+    public const double LEGO_WIDTH = 20; // X or Z Size
+    public const double LEGO_DEPTH = 20; // X or Z Size
+
+    // Stud Measurements
+    public const double STUD_HEIGHT = 4;
+    public const double STUD_RADIUS = 6;
+    public const double STUD_DIAMETER = 12;
+
+    // distances between two adjacent studs
+    public const double DISTANCE_BETWEEN_STUD_CENTERS = 40;
+    public const double DISTANCE_BETWEEN_STUD_EDGES = 8;
+
+    /**************************/
+    /* 1x1 Brick Measurements */
+    /**************************/
+    public const double BRICK_HEIGHT = 24; // Y Size
+
+    /**************************/
+    /* 1x1 Plate Measurements */
+    /**************************/
+    public const double PLATE_HEIGHT = 8; // Y Size
+}
---
+++ Assets/Scripts/LegoMeasurements/LegoMeasurementsMM.cs
@@ -0,0 +1,36 @@
+﻿public class LegoInfoMM
+{
+    /* 
+      Below are defined sizes for lego pieces. 
+      All measurements are in millimeters(mm).   
+    */
+
+    // conversion from ldu to mm
+    public const double MM_TO_LDU_RATIO = 0.4;
+
+    /**************************/
+    /* 1x1 Lego Measurements */
+    /**************************/
+    // Standard Lego Measurements
+    public const double LEGO_WIDTH = 8; // X or Z Size
+    public const double LEGO_DEPTH = 8; // X or Z Size
+
+    // Stud Measurements
+    public const double STUD_HEIGHT = 1.6;
+    public const double STUD_RADIUS = 2.4;
+    public const double STUD_DIAMETER = 4.8;
+
+    // distances between two adjacent studs
+    public const double DISTANCE_BETWEEN_STUD_CENTERS = 8;
+    public const double DISTANCE_BETWEEN_STUD_EDGES = 3.2;
+
+    /**************************/
+    /* 1x1 Brick Measurements */
+    /**************************/
+    public const double BRICK_HEIGHT_WITHOUT_STUD = 9.6; // Y Size
+
+    /**************************/
+    /* 1x1 Plate Measurements */
+    /**************************/
+    public const double PLATE_HEIGHT_WITHOUT_STUD = 3.2; // Y Size
+}
