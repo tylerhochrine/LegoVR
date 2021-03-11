708abb732eda954ebf181ff2d79a0d67b6d55b3b
Aika Washington <aikawashington867@gmail.com>
2021-03-10 23:59:00 -0500

    added snap sounds to the bricks
    add sound script, Audio Source to scene. snapSound in assets

---
+++ Assets/Scripts/sound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class sound : MonoBehaviour
+{
+    
+    AudioSource snapSound;
+
+
+   
+
+
+    void Awake () {
+
+        snapSound = GetComponent<AudioSource>();
+    }
+
+
+    void OnCollisionEnter (Collision coll)
+    {
+       snapSound.Play();
+    }
+}
