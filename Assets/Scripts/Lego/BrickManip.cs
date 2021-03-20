dbe1b4308ef1a70086a0f6cdee904fd152b6cb86
Tyler Hochrine <thochri@clemson.edu>
2021-03-20 04:09:11 -0400

    Added 'Remove Brick' Button Functionality on Menu

---
+++ Assets/Scripts/Lego/BrickManip.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickManip : MonoBehaviour
+{
+
+    public GameObject prefab;
+    GameObject cloneBrick = null;
+
+    public void addBrick()
+    {
+        cloneBrick = Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    public void removeBrick()
+    {
+        if(cloneBrick != null)
+        {
+            cloneBrick = cloneBrick.transform.GetChild(0).GetChild(0).gameObject;
+            cloneBrick.SetActive(false);
+            //Destroy(cloneBrick);
+        }
+    }
+
+    public void setBrick(GameObject b)
+    {
+        cloneBrick = b;
+    }
+}
