4ffbdf1595511ccc92440ddca6ac569b244aab46
Aika Washington <aikawashington867@gmail.com>
2021-04-16 08:20:25 -0400

    scene speaker, spetialized music

---
+++ Assets/footstep.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+ 
+public class footstep : MonoBehaviour
+{
+    AudioSource foot;
+    private bool IsMoving;
+ 
+    void Start()
+    {
+        foot = gameObject.GetComponent<AudioSource>();
+    }
+ 
+    void Update()
+    {
+        if (Input.GetAxis("Vertical") < 0) IsMoving = true; // better use != 0 here for both directions
+        else IsMoving = false;
+ 
+        if (IsMoving && !foot.isPlaying) foot.Play(); // if player is moving and audiosource is not playing play it
+        if (!IsMoving) foot.Stop(); // if player is not moving and audiosource is playing stop it
+    }
+}
---
+++ Assets/musicplay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class musicplay : MonoBehaviour
+
+{
+
+    AudioSource piano;
+
+
+    void Awake(){
+        piano = GetComponent<AudioSource>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        piano.loop = true;
+        piano.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
